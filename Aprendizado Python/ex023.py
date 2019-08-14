98bef8950eee26e8f3b8ce85530a2685803ae86a
wjurity <william.jurity17@gmail.com>
2019-08-14 20:21:21 -0300

    ex 05 - 17

---
+++ Aprendizado Python/ex023.py
@@ -0,0 +1,5 @@
+n = input('Digite um número de 0 a 9999: ')
+print(n[0])
+print(n[1])
+print(n[2])
+print(n[3])
---
+++ Aprendizado Python/ex024.py
@@ -0,0 +1,3 @@
+cidade = input('Digite o nome de uma cidade: ')
+cidade = cidade.split()
+print('SANTO' in cidade[0])
