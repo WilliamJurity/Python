76eb67bde0d5e456c73a6bdd783c18e677d5500e
williamjurity <william.jurity@outlook.com>
2019-08-15 17:38:54 -0300

    Exercicios 5-33

---
+++ Aprendizado Python/ex033.py
@@ -0,0 +1,12 @@
+n1 = input('Digite o primeiro número: ')
+while n1.strip().isnumeric() == False:
+    n1 = input('Digite um número válido: ')
+n1 = int(n1)
+n2 = input('Digite o segundo número: ')
+while n2.strip().isnumeric() == False:
+    n2 = input('Digite um número válido: ')
+n2 = int(n2)
+n3 = input('Digite um número válido: ')
+while n3.strip().isnumeric() == False:
+    n3 = input('Digite um número válido: ')
+n3 = int(n3)
