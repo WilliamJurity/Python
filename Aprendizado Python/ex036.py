b57dd66a81059666b1e2e255d85dcd4c7254fad2
wjurity <william.jurity17@gmail.com>
2019-08-17 16:53:52 -0300

    ex 05 - 46

---
+++ Aprendizado Python/ex036.py
@@ -0,0 +1,24 @@
+vlcasa = input('Digite o valor da casa: ')
+while vlcasa.strip().isnumeric() == False:
+    vlcasa = input('Digite um valor válido')
+vlcasa = float(vlcasa)
+
+sl = input('Digite o seu salário: ')
+while sl.strip().isnumeric() == False:
+    sl = input('Digite um valor válido: ')
+sl = float(sl)
+
+parcelas = input('Digite o valor das parcelas: ')
+while parcelas.strip().isnumeric() == False:
+    parcelas = input('Digite um valor válido: ')
+parcelas = float(parcelas)
+
+prestacao = vlcasa / parcelas
+print('O valor de cada prestação da sua casa é {:.2f}'.format(prestacao))
+
+x = sl * 30 / 100
+
+if prestacao > x:
+    print('Situação: NEGADO! Motivo: As prestações de {:.2f}R$ excedem 30% do seu salário mensal ({:.2f}R$).'.format(prestacao, x))
+else:
+    print('Situação: ACEITO!')
---
+++ Aprendizado Python/ex046.py
@@ -0,0 +1,6 @@
+import time
+
+for t in range(10, 0, -1):
+    print(t)
+    time.sleep(1)
+print('BUUUUUMMMMMMM')
